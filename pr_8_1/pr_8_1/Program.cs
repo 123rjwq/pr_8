fe1ce97895d9965bd282e553a8592c43d7433330
123rjwq <145851361+123rjwq@users.noreply.github.com>
2023-12-18 04:03:29 +0300

    Add files via upload

---
+++ pr_8_1/pr_8_1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace pr_8_1
+{
+    class Program
+    {
+        class Computer
+        {
+            private string modelName;
+            // Модель компьютера
+            private int ramSize;
+            // Размер оперативной памяти
+
+            public string ModelName
+            {
+                get { return modelName; }
+                set { modelName = (value != "") ? value : "noName"; }
+            }
+
+            public int RamSize
+            {
+                get { return ramSize; }
+                set { ramSize = (value < 500) ? 640 : value; }
+            }
+
+            public Computer() { }
+
+            public Computer(string modelName, int ramSize)
+            {
+                this.ModelName = modelName;
+                this.RamSize = ramSize;
+            }
+
+            public virtual void Start()
+            {
+                Console.WriteLine("{0} работает, память = {1}", ModelName, RamSize);
+            }
+
+            public virtual void End()
+            {
+                Console.WriteLine("{0} выключается", ModelName);
+            }
+        }
+
+        class Notebook : Computer
+        {
+            private int batteryTime;
+            // Время работы от батареи
+
+            public int BatteryTime
+            {
+                get { return batteryTime; }
+                set { batteryTime = (value < 10) ? 15 : value; }
+            }
+
+            public Notebook() { }
+
+            public Notebook(string modelName, int ramSize, int batteryTime) : base(modelName, ramSize)
+            {
+                this.BatteryTime = batteryTime;
+            }
+
+            public override void End()
+            {
+                Console.WriteLine("{0} выключается, заряд {1} мин", ModelName, BatteryTime);
+            }
+        }
+
+        class Netbook : Notebook
+        {
+            private int weight;
+            // Вес нетбука
+
+            public int Weight
+            {
+                get { return weight; }
+                set { weight = (value < 30) ? 10 : value; }
+            }
+
+            public Netbook(string modelName, int ramSize, int batteryTime, int weight) : base(modelName, ramSize, batteryTime)
+            {
+                this.Weight = weight;
+            }
+
+            // Переопределенный метод Start с модификатором override
+            public override void Start()
+            {
+                Console.WriteLine("{0} весит {1} кг", ModelName, Weight);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Notebook nb = new Notebook("Asus", 1024, 120);
+            nb.Start();
+            // Запускаем ноутбук
+            nb.End();
+            // Выключаем ноутбук
+
+            Computer comp2 = new Notebook("Dell", 4096, 30);
+            comp2.Start();
+            // Запускаем компьютер
+            comp2.End();
+            // Выключаем компьютер
+
+            Netbook netb1 = new Netbook("HP", 4096, 120, 20);
+            netb1.Start();
+            // Запускаем нетбук
+            netb1.End();
+            // Выключаем нетбук
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
---
+++ pr_8_2/pr_8_2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace Пр_8_ИндивРаб_Старостин
+{
+    class Transport
+    {
+        public string Model { get; set; }
+        // Переменная, хранящая модель транспорта.
+        public int Speed { get; set; }
+        // Переменная, хранящая скорость транспорта.
+        public double Mass { get; set; }
+        // Переменная, хранящая массу транспорта.
+
+        public void Start()
+            // Метод, запускающий движение транспорта.
+        {
+            Console.WriteLine("Транспорт начал движение.");
+        }
+
+        public void Stop()
+            // Метод, останавливающий транспорт.
+        {
+            Console.WriteLine("Транспорт остановился.");
+        }
+
+        public void ShowInfo()
+            // Метод, выводящий информацию о транспорте.
+        {
+            Console.WriteLine("{0} модель", Model);
+            Console.WriteLine("Скорость {0} км/ч ", Speed);
+            Console.WriteLine("Масса {0} кг", Mass);
+        }
+    }
+
+    class Avto : Transport
+        // Класс, описывающий автомобиль.
+    {
+        public Avto(string model, int speed, double mass)
+            // Конструктор класса автомобиль.
+        {
+            Model = model;
+            Speed = speed;
+            Mass = mass;
+        }
+    }
+
+    class Moto : Transport
+        // Класс, описывающий мотоцикл.
+    {
+        public Moto(string model, int speed, double mass) 
+            // Конструктор класса мотоцикл.
+        {
+            Model = model;
+            Speed = speed;
+            Mass = mass;
+        }
+    }
+
+    class Velo : Transport
+        // Класс, описывающий велосипед.
+    {
+        public Velo(string model, int speed, double mass)
+            // Конструктор класса велосипед.
+        {
+            Model = model;
+            Speed = speed;
+            Mass = mass;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Avto legkovoyAvto = new Avto("Легковой автомобиль", 120, 1500); 
+            // Создание объекта автомобиль с заданными параметрами.
+            Moto motocycle = new Moto("Мотоцикл", 180, 250);
+            // Создание объекта мотоцикл с заданными параметрами.
+            Velo bicycle = new Velo("Велосипед", 20, 10);
+            // Создание объекта велосипед с заданными параметрами.
+
+            Console.WriteLine("Информация об автомобиле:");
+            // Вывод информации об автомобиле.
+            legkovoyAvto.ShowInfo();
+            // Вызов метода ShowInfo() для объекта автомобиль.
+            legkovoyAvto.Start();
+            // Запуск движения автомобиля.
+            legkovoyAvto.Stop();
+            // Остановка автомобиля.
+
+            Console.WriteLine("\nИнформация о мотоцикле:");
+            // Вывод информации о мотоцикле.
+            motocycle.ShowInfo();
+            // Вызов метода ShowInfo() для объекта мотоцикл.
+            motocycle.Start();
+            // Запуск движения мотоцикла.
+            motocycle.Stop();
+            // Остановка мотоцикла.
+
+            Console.WriteLine("\nИнформация о велосипеде:");
+            // Вывод информации о велосипеде.
+            bicycle.ShowInfo();
+            // Вызов метода ShowInfo() для объекта велосипед.
+            bicycle.Start();
+            // Запуск движения велосипеда.
+            bicycle.Stop();
+            // Остановка велосипеда.
+        }
+    }
+}
